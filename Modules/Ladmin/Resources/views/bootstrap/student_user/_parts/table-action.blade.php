430abd7e8a97e80e5af23ee40a3fbec53e01fcf2
RyoGenesis <rtunggala18@gmail.com>
2023-08-15 16:37:10 +0700

    update

--- Modules/Ladmin/Resources/views/bootstrap/student_user/_parts/table-action.blade.php
+++ Modules/Ladmin/Resources/views/bootstrap/student_user/_parts/table-action.blade.php
@@ -3,11 +3,11 @@
 @endphp
 <div class="d-grid gap-2">
   @can(['ladmin.student_user.index'])
-      <a href="{{ route('ladmin.student_user.edit', [$id, 'back' => $back]) }}" class="btn btn-sm btn-primary">
-        Edit
-      </a>
-
       @if ($deleted_at == null)
+        <a href="{{ route('ladmin.student_user.edit', [$id, 'back' => $back]) }}" class="btn btn-sm btn-primary">
+          Edit
+        </a>
+        
         <button type="button" class="btn btn-sm btn-danger" data-id="{{$id}}" data-bs-toggle="modal" data-bs-target="#delete-modal">
           Deactivate
         </button>
